f8b4ce2cdda1ec936e582b38016999465ea28380
Stefan Dlugolinsky <stefan.dlugolinsky@savba.sk>
2025-11-14 09:19:27 +0100

    Create file

---
+++ workflows/edcae73b8ad544c3a211fc8ef0da8576.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from dateutil.parser import parse
+from airflow.sdk import DAG
+from domino.task import Task
+
+dag_config_0 = {'start_date': '2025-11-14T08:19:00', 'schedule': None, 'catchup': False, 'dag_id': 'edcae73b8ad544c3a211fc8ef0da8576'}
+
+# Parse datetime values
+dt_keys = ['start_date', 'end_date']
+dag_config = { k: (v if k not in dt_keys else parse(v)) for k, v in dag_config_0.items()}
+dag_config = {**dag_config, 'is_paused_upon_creation': False}
+
+with DAG(**dag_config) as dag:
+    SleepPiece_ddc87bc790c141038cdd01841138b2a9 = Task(
+        dag,
+        task_id='SleepPiece_ddc87bc790c141038cdd01841138b2a9',
+        workspace_id=1,
+        workflow_shared_storage={'source': 'None', 'mode': 'Read/Write', 'provider_options': {}},
+        container_resources={'requests': {'cpu': '100.0m', 'memory': '128.0Mi'}, 'limits': {'cpu': '100.0m', 'memory': '128.0Mi'}, 'use_gpu': False},
+        piece={'name': 'SleepPiece', 'source_image': 'ghcr.io/iisas/default_domino_pieces:0.9.0-group0', 'repository_url': 'https://github.com/IISAS/default_domino_pieces', 'repository_version': '0.9.0'},
+        piece_input_kwargs={'sleep_time': 1}
+    )()
+
