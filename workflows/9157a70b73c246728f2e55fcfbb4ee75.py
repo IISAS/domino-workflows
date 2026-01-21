42c087d4067539dcdf12b5c588902ec2e530ea9e
Martin Šeleng <pakoselo@users.noreply.github.com>
2026-01-21 14:20:30 +0100

    Create file

---
+++ workflows/9157a70b73c246728f2e55fcfbb4ee75.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from dateutil.parser import parse
+from airflow.sdk import DAG
+from domino.task import Task
+
+dag_config_0 = {'start_date': '2026-01-21T13:20:00', 'schedule': None, 'catchup': False, 'dag_id': '9157a70b73c246728f2e55fcfbb4ee75'}
+
+# Parse datetime values
+dt_keys = ['start_date', 'end_date']
+dag_config = { k: (v if k not in dt_keys else parse(v)) for k, v in dag_config_0.items()}
+dag_config = {**dag_config, 'is_paused_upon_creation': False}
+
+with DAG(**dag_config) as dag:
+    SleepPiece_051688b3083e434cbead3e58497cdc23 = Task(
+        dag,
+        task_id='SleepPiece_051688b3083e434cbead3e58497cdc23',
+        workspace_id=1,
+        workflow_shared_storage={'source': 'None', 'mode': 'Read/Write', 'provider_options': {}},
+        container_resources={'requests': {'cpu': '100.0m', 'memory': '128.0Mi'}, 'limits': {'cpu': '100.0m', 'memory': '128.0Mi'}, 'use_gpu': False},
+        piece={'name': 'SleepPiece', 'source_image': 'ghcr.io/tauffer-consulting/default_domino_pieces:0.8.1-group0', 'repository_url': 'https://github.com/Tauffer-Consulting/default_domino_pieces', 'repository_version': '0.8.1'},
+        piece_input_kwargs={'sleep_time': 1}
+    )()
+    SleepPiece_9a274bfeaa674202949da19192cc3411 = Task(
+        dag,
+        task_id='SleepPiece_9a274bfeaa674202949da19192cc3411',
+        workspace_id=1,
+        workflow_shared_storage={'source': 'None', 'mode': 'Read/Write', 'provider_options': {}},
+        container_resources={'requests': {'cpu': '100.0m', 'memory': '128.0Mi'}, 'limits': {'cpu': '100.0m', 'memory': '128.0Mi'}, 'use_gpu': False},
+        piece={'name': 'SleepPiece', 'source_image': 'ghcr.io/tauffer-consulting/default_domino_pieces:0.8.1-group0', 'repository_url': 'https://github.com/Tauffer-Consulting/default_domino_pieces', 'repository_version': '0.8.1'},
+        piece_input_kwargs={'sleep_time': 1}
+    )()
+
+    SleepPiece_9a274bfeaa674202949da19192cc3411.set_upstream([globals()[t] for t in ['SleepPiece_051688b3083e434cbead3e58497cdc23']])
