3c16abb665db7a0112a7d10f0b21aaaeaf417d98
Martin Šeleng <pakoselo@users.noreply.github.com>
2026-01-22 09:18:18 +0100

    Update 540d2983e01e43949d7d499f72d8a157.py

--- workflows/540d2983e01e43949d7d499f72d8a157.py
+++ workflows/540d2983e01e43949d7d499f72d8a157.py
@@ -11,7 +11,7 @@ dag_config = { k: (v if k not in dt_keys else parse(v)) for k, v in dag_config_0
 dag_config = {**dag_config, 'is_paused_upon_creation': False}
 
 with DAG(**dag_config) as dag:
-    HttpReques_27dd10b0cd4b41a5bb118468b0a3c60b = Task(
+    httpreques-27dd10b0cd4b41a5bb118468b0a3c60b = Task(
         dag,
         task_id='HttpReques_27dd10b0cd4b41a5bb118468b0a3c60b',
         workspace_id=1,
@@ -20,7 +20,7 @@ with DAG(**dag_config) as dag:
         piece={'name': 'HttpRequestPiece', 'source_image': 'ghcr.io/tauffer-consulting/default_domino_pieces:0.8.1-group0', 'repository_url': 'https://github.com/Tauffer-Consulting/default_domino_pieces', 'repository_version': '0.8.1'},
         piece_input_kwargs={'url': 'https://images.pexels.com/photos/4055758/pexels-photo-4055758.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 'method': 'GET', 'bearer_token': None, 'body_json_data': '{\n    "key_1": "value_1",\n    "key_2": "value_2"\n}\n'}
     )()
-    ImageFilte_7a0d7be2528e4149831ad0a4308dcb62 = Task(
+    imagefilte-7a0d7be2528e4149831ad0a4308dcb62 = Task(
         dag,
         task_id='ImageFilte_7a0d7be2528e4149831ad0a4308dcb62',
         workspace_id=1,
@@ -30,4 +30,4 @@ with DAG(**dag_config) as dag:
         piece_input_kwargs={'input_image': {'type': 'fromUpstream', 'upstream_task_id': 'HttpReques_27dd10b0cd4b41a5bb118468b0a3c60b', 'output_arg': 'base64_bytes_data'}, 'sepia': False, 'black_and_white': True, 'brightness': False, 'darkness': False, 'contrast': False, 'red': False, 'green': False, 'blue': False, 'cool': True, 'warm': False, 'output_type': 'both'}
     )()
 
-    ImageFilte_7a0d7be2528e4149831ad0a4308dcb62.set_upstream([globals()[t] for t in ['HttpReques_27dd10b0cd4b41a5bb118468b0a3c60b']])
+    imagefilte-7a0d7be2528e4149831ad0a4308dcb62.set_upstream([globals()[t] for t in ['httpreques-27dd10b0cd4b41a5bb118468b0a3c60b']])
