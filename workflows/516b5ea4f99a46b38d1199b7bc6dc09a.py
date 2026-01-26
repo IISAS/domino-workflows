474d26fdf41b97b8ea476fe314a50796836eb152
Martin Šeleng <pakoselo@users.noreply.github.com>
2026-01-26 18:31:17 +0100

    Update 516b5ea4f99a46b38d1199b7bc6dc09a.py

--- workflows/516b5ea4f99a46b38d1199b7bc6dc09a.py
+++ workflows/516b5ea4f99a46b38d1199b7bc6dc09a.py
@@ -13,14 +13,14 @@ dag_config = {**dag_config, 'is_paused_upon_creation': False}
 with DAG(**dag_config) as dag:
     HttpReques_27dd10b0cd4b41a5bb118468b0a3c60b = Task(
         dag,
-        task_id='HttpReques_27dd10b0cd4b41a5bb118468b0a3c60b',
+        task_id='HttpReques-27dd10b0cd4b41a5bb118468b0a3c60b',
         workspace_id=1,
         workflow_shared_storage={'source': 'Local', 'mode': 'Read/Write', 'provider_options': {}},
         container_resources={'requests': {'cpu': '100.0m', 'memory': '128.0Mi'}, 'limits': {'cpu': '100.0m', 'memory': '128.0Mi'}, 'use_gpu': False},
         piece={'name': 'HttpRequestPiece', 'source_image': 'ghcr.io/tauffer-consulting/default_domino_pieces:0.8.1-group0', 'repository_url': 'https://github.com/Tauffer-Consulting/default_domino_pieces', 'repository_version': '0.8.1'},
         piece_input_kwargs={'url': 'https://images.pexels.com/photos/4055758/pexels-photo-4055758.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 'method': 'GET', 'bearer_token': None, 'body_json_data': '{\n    "key_1": "value_1",\n    "key_2": "value_2"\n}\n'}
     )()
-    ImageFilte_7a0d7be2528e4149831ad0a4308dcb62 = Task(
+    ImageFilte-7a0d7be2528e4149831ad0a4308dcb62 = Task(
         dag,
         task_id='ImageFilte_7a0d7be2528e4149831ad0a4308dcb62',
         workspace_id=1,
