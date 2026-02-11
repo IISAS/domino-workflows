78b5834b17e2f049ad9dcc2039cab35fdd5868e7
Martin Šeleng <pakoselo@users.noreply.github.com>
2026-02-11 10:02:27 +0100

    Update bb7688be521a4e9b8fdc93c4054854f2.py

--- workflows/bb7688be521a4e9b8fdc93c4054854f2.py
+++ workflows/bb7688be521a4e9b8fdc93c4054854f2.py
@@ -34,7 +34,7 @@ with DAG(**dag_config) as dag:
         task_id='saveimageo_96574846d789416c8f4f8885bea5411c',
         workspace_id=1,
         workflow_shared_storage={'source': 'Local', 'mode': 'Read/Write', 'provider_options': {}},
-        container_resources={'requests': {'cpu': '100.0m', 'memory': '128.0Mi'}, 'limits': {'cpu': '100.0m', 'memory': '128.0Mi'}, 'use_gpu': False},
+        container_resources={'requests': {'cpu': '100.0m', 'memory': '1024.0Mi'}, 'limits': {'cpu': '100.0m', 'memory': '1024.0Mi'}, 'use_gpu': False},
         piece={'name': 'SaveImagePiece', 'source_image': 'ghcr.io/iisas/default_domino_pieces:0.9.0-group0', 'repository_url': 'https://github.com/IISAS/default_domino_pieces', 'repository_version': '0.9.0'},
         piece_input_kwargs={'base64_data': {'type': 'fromUpstream', 'upstream_task_id': 'imagefilte-03288d1e8caf4426932809fadcb7edb7', 'output_arg': 'image_base64_string'}}
     )()
