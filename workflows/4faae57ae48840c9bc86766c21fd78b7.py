6775c4355d9d5ea3c0e9d2211d3ddfc9629dae58
Martin Šeleng <pakoselo@users.noreply.github.com>
2026-03-04 08:53:07 +0100

    Create file

---
+++ workflows/4faae57ae48840c9bc86766c21fd78b7.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from dateutil.parser import parse
+from airflow.sdk import DAG
+from domino.task import Task
+
+dag_config_0 = {'start_date': '2026-03-04T07:53:00', 'schedule': None, 'catchup': False, 'dag_id': '4faae57ae48840c9bc86766c21fd78b7'}
+
+# Parse datetime values
+dt_keys = ['start_date', 'end_date']
+dag_config = { k: (v if k not in dt_keys else parse(v)) for k, v in dag_config_0.items()}
+dag_config = {**dag_config, 'is_paused_upon_creation': False}
+
+with DAG(**dag_config) as dag:
+    CreateIgni_4ea8ce104917429ab3a83e115c3748c6 = Task(
+        dag,
+        task_id='CreateIgni_4ea8ce104917429ab3a83e115c3748c6',
+        workspace_id=4,
+        workflow_shared_storage={'source': 'Local', 'mode': 'Read/Write', 'provider_options': {}},
+        container_resources={'requests': {'cpu': '100.0m', 'memory': '128.0Mi'}, 'limits': {'cpu': '100.0m', 'memory': '128.0Mi'}, 'use_gpu': False},
+        piece={'name': 'CreateIgnitionPiece', 'source_image': 'ghcr.io/valaseklukas-svg/domino-farsite-pieces:0.1.33-group0', 'repository_url': 'https://github.com/valaseklukas-svg/domino-farsite-pieces', 'repository_version': '0.1.33'},
+        piece_input_kwargs={'gps_text': '48.117324441088336, 17.090540411963868'}
+    )()
+    EvaluatePe_d821b9875df5409c800b8668f8968616 = Task(
+        dag,
+        task_id='EvaluatePe_d821b9875df5409c800b8668f8968616',
+        workspace_id=4,
+        workflow_shared_storage={'source': 'Local', 'mode': 'Read/Write', 'provider_options': {}},
+        container_resources={'requests': {'cpu': '100.0m', 'memory': '128.0Mi'}, 'limits': {'cpu': '100.0m', 'memory': '128.0Mi'}, 'use_gpu': False},
+        piece={'name': 'EvaluatePerimeterPiece', 'source_image': 'ghcr.io/valaseklukas-svg/domino-farsite-pieces:0.1.33-group1', 'repository_url': 'https://github.com/valaseklukas-svg/domino-farsite-pieces', 'repository_version': '0.1.33'},
+        piece_input_kwargs={'outputs_zip_path': {'type': 'fromUpstream', 'upstream_task_id': 'ExecuteFar_24e81841ce794a9e9b758ce1db8f222e', 'output_arg': 'outputs_zip_path'}, 'ignition_shp_path': {'type': 'fromUpstream', 'upstream_task_id': 'CreateIgni_4ea8ce104917429ab3a83e115c3748c6', 'output_arg': 'ignition_shp_path'}, 'buffer_shp_path': '/home/shared_storage/fire/buffer.shp', 'api_model_name': 'Model 2'}
+    )()
+    ExecuteFar_24e81841ce794a9e9b758ce1db8f222e = Task(
+        dag,
+        task_id='ExecuteFar_24e81841ce794a9e9b758ce1db8f222e',
+        workspace_id=4,
+        workflow_shared_storage={'source': 'Local', 'mode': 'Read/Write', 'provider_options': {}},
+        container_resources={'requests': {'cpu': '500.0m', 'memory': '512.0Mi'}, 'limits': {'cpu': '500.0m', 'memory': '512.0Mi'}, 'use_gpu': False},
+        piece={'name': 'ExecuteFarsitePiece', 'source_image': 'ghcr.io/valaseklukas-svg/domino-farsite-pieces:0.1.33-group0', 'repository_url': 'https://github.com/valaseklukas-svg/domino-farsite-pieces', 'repository_version': '0.1.33'},
+        piece_input_kwargs={'lcp_path': '/home/shared_storage/fire/final.lcp', 'inputs_path': '/home/shared_storage/fire/Zavada.input', 'ignition_shp_path': {'type': 'fromUpstream', 'upstream_task_id': 'CreateIgni_4ea8ce104917429ab3a83e115c3748c6', 'output_arg': 'ignition_shp_path'}, 'barrier_shp_path': '0', 'output_basename': 'farsite_run', 'outputs_type': 1}
+    )()
+
+    EvaluatePe_d821b9875df5409c800b8668f8968616.set_upstream([globals()[t] for t in ['ExecuteFar_24e81841ce794a9e9b758ce1db8f222e', 'CreateIgni_4ea8ce104917429ab3a83e115c3748c6']])
+    ExecuteFar_24e81841ce794a9e9b758ce1db8f222e.set_upstream([globals()[t] for t in ['CreateIgni_4ea8ce104917429ab3a83e115c3748c6']])
